194430c4c4cbf49661e28dc19773a97856b621fd
zxuqian <zxuqian@gmail.com>
2019-12-15 11:59:06 +0800

    Update About Me

--- src/HomePage/AboutMe/index.js
+++ src/HomePage/AboutMe/index.js
@@ -12,7 +12,10 @@ function AboutMe(props) {
         我热爱学习，在高考失利后，我没有放弃学习，从专科到专升本到美国硕士，我以求知为己任，不断充实自己，有着强大的学习与研究能力。由于对技术的热爱，业余时间我会关注最新的技术动向，去学习感兴趣的新技术，以丰富自己的技术栈，在遇到职业变动或希望尝试不同方向时，能很快适应。
         {/* </p> */}
         {/* <p> */}
-        我目前专注于前端和全栈开发，如您有需要，请与我联系。
+        {/* </p> */}
+        {/* <p> */}
+        我目前专注于前端和全栈开发，如您有需要，可到页面底部
+        <strong>查看联系方式。</strong>
       </p>
     </Container>
   );
@@ -24,6 +27,10 @@ const Container = styled.div`
   padding: 0 1.1em;
   font-size: 1.1em;
   line-height: 1.8em;
+
+  strong {
+    color: ${props => props.theme.main.primary};
+  }
 `;
 
 export default AboutMe;
